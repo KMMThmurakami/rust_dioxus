3c6a9ed4a9dd8ba79b55542fbae0915d5241a457
hideaki.murakami <hideaki.murakami@nijibox.co.jp>
2024-07-10 11:58:39 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,13 @@ enum Route {
     Blog { id: i32 },
 }
 
+#[derive(Debug)]
+enum CalcFlag {
+    Add,
+    Sub,
+    Multiple,
+}
+
 fn main() {
     // Init logger
     dioxus_logger::init(Level::INFO).expect("failed to init logger");
@@ -43,6 +50,22 @@ fn Blog(id: i32) -> Element {
 fn Home() -> Element {
     let mut count: Signal<i32> = use_signal(|| 0);
 
+    let mut update_count = move |flg: CalcFlag| {
+        let new_count: Option<i32>;
+        match flg {
+            CalcFlag::Add => new_count = count().checked_add(1),
+            CalcFlag::Sub => new_count = count().checked_sub(1),
+            CalcFlag::Multiple => new_count = count().checked_mul(2),
+        }
+        match new_count {
+            Some(new_value) => {
+                count.set(new_value);
+                log(&format!("Count changed to {}", new_value));
+            },
+            None => log("Error: Overflow detected when changing the count"),
+        }
+    };
+
     rsx! {
         Link {
             to: Route::Blog {
@@ -59,33 +82,9 @@ fn Home() -> Element {
             div { "u128_max = {std::u128::MAX}" }
             div {
                 class: "setButton",
-                button { onclick: move |_| {
-                    match count().checked_add(1) {
-                        Some(new_count) => {
-                            count.set(new_count);
-                            log(&format!("Count increased to {}", new_count));
-                        },
-                        None => log("Error: Overflow detected when incrementing the count"),
-                    }
-                }, "Up high!" }
-                button { onclick: move |_| {
-                    match count().checked_sub(1) {
-                        Some(new_count) => {
-                            count.set(new_count);
-                            log(&format!("Count decreased to {}", new_count));
-                        },
-                        None => log("Error: Overflow detected when decrementing the count"),
-                    }
-                }, "Down low!" }
-                button { onclick: move |_| {
-                    match count().checked_mul(2) {
-                        Some(new_count) => {
-                            count.set(new_count);
-                            log(&format!("Count doubled to {}", new_count));
-                        },
-                        None => log("Error: Overflow detected when doubling the count"),
-                    }
-                }, "Double!" }
+                button { onclick: move |_| update_count(CalcFlag::Add), "+1" }
+                button { onclick: move |_| update_count(CalcFlag::Sub), "-1" }
+                button { onclick: move |_| update_count(CalcFlag::Multiple), "×2" }
                 button { onclick: move |_| count.set(0), "Reset!" }
             }
         }
